610ee3e9794c21e270ac046aaf66af4501dd7e41
Aliyu Efe <aliyuefe9501@gmail.com>
2026-04-06 19:09:15 +0100

    initial commit

---
+++ app/constant.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Newsletters',
+    href: '/newsletters',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+  },
+  {
+    name: 'About SDGs',
+    href: '/about-sdgs',
+  },
+  {
+    name: 'Past Events',
+    href: '/past-events',
+  },
+];
