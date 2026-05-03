a005ea6fb5274526a97277bfd420f7106c5f7d97
Aliyu Efe <aliyuefe9501@gmail.com>
2026-05-03 18:28:45 +0100

    feat:update youtube playlist

---
+++ constants/youtube.constant.ts
@@ -0,0 +1,30 @@
+export const videos = [
+  {
+    id: '-NPtdqQSOTE',
+    title: `Women of the Stars part 2`,
+    duration: '125 min',
+  },
+  {
+    id: 'jg7KIsZ2IKk',
+    title: `LUNA WOMEN OF THE STARS WEBINAR PT 1`,
+    duration: '58 min',
+  },
+  {
+    id: 'H8DMp_zvqV0',
+    title: `Africa's Space Future | Webinar Highlights | LUNA PT 3`,
+    duration: '45 min',
+  },
+  {
+    id: 'KFsQqJvH5XM',
+    title: `Africa's Space Future | Webinar Highlights | LUNA PT 2`,
+    duration: '41 min',
+  },
+
+  {
+    id: 'TlNsm1QmWR0',
+    title: 'The Future of Space in Africa | LUNA Webinar Highlights PT 1',
+    duration: '28 min',
+  },
+];
+
+export const channelName = '@LunaAfriSpace';
